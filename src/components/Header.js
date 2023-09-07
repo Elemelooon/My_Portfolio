95f35017fe1282c383eb0eb96b6e3b8cc4265cd8
elemelooon <rissaleijavier@gmail.com>
2023-09-08 02:08:25 +0800

    typewriter change

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,12 @@ const Header = () => {
         <div className="container my-5 p-5">
             <div className="row">
                 <div className="col-12 col-md-8">
-                <div class="typewriter">
+                <div className="row">
+                <div class="typewriter col-md-7 col-12">
                 <h1>Hi! I'm Leii!</h1>
-                <p className="h4 py-2 intro">Welcome to my Aspiring Programmer's Portfolio! I'm excited to present my journey into the world of programming and software development. As a newbie programmer, I've embarked on this incredible adventure to explore the vast realms of coding, problem-solving, and creating software solutions that can make a positive impact on our digital world.</p>
                 </div>
+                </div>
+                <p className="h4 py-2 intro">Welcome to my Aspiring Programmer's Portfolio! I'm excited to present my journey into the world of programming and software development. As a newbie programmer, I've embarked on this incredible adventure to explore the vast realms of coding, problem-solving, and creating software solutions that can make a positive impact on our digital world.</p>
                 </div>
                 <div className="col-12 col-md-4">
                 <img src={Icon} alt="" className="w-100 xiaobirb rounded-circle"/>
